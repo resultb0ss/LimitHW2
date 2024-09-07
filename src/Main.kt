56a88bcc5bbd1405993781168f14828def40904d
Admin <reteamboss@gmail.com>
2024-09-07 15:15:05 +0300

    2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,14 @@
 fun main() {
-    println("Hello World!")
+
+    var array = arrayOf(1,2,3,4,5,6,7,8)
+    println(GenericMethod().isItIncluded(1,array))
+    println(GenericMethod().isItIncluded(10,array))
+}
+
+class GenericMethod(){
+
+    fun <T:Comparable<T>> isItIncluded(ob: T, array: Array<T>) : Boolean {
+
+        return array.contains(ob)
+    }
 }
